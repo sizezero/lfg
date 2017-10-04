c4ba7831cad1c8959edf5b4ce77d83e2674523a7
Robert Kleemann <robert@kleemann.org>
2017-10-03 17:00:01 -0700

    simple cards in landscape mode

--- src/armor.rb
+++ src/armor.rb
@@ -1,6 +1,15 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
-  text str: 'Hello, World!'
-  save format: :png
+data = Squib.csv file: 'armor.csv'
+
+# default size for Deck.new appears to be for portrait poker card
+# inverting for landscape
+Squib::Deck.new(cards: data['name'].size, width: 1125, height: 825, layout: 'layouts/layout.yml') do
+  background color: 'white'
+  rect layout: 'cut'
+  rect layout: 'safe'
+  text str: data['name'], layout: 'title'
+
+  save_sheet prefix: 'armor', columns: 3, dir: 'output'
+  save_pdf trim: 37.5, dir: 'output'
 end
