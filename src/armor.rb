c94e96f2c102e30d6f67bbd92264a7eb5ff52ae5
Robert Kleemann <robert@kleemann.org>
2017-10-04 16:36:27 -0700

    added colored medal

--- src/armor.rb
+++ src/armor.rb
@@ -12,7 +12,7 @@ Squib::Deck.new(cards: data['name'].size, width: 1125, height: 825, layout: 'lay
 
   text str: data['name'], layout: 'title'
   text str: data['stars'], layout: 'stars'
-  text str: data['vp'].map { |s| s&.gsub(':vp:','VP') }, layout: 'vp'
+  text markup: true, str: data['vp'].map { |s| s&.gsub(':vp:','<span foreground="#5e11a6">🎖</span>') }, layout: 'vp'
 
   save_sheet prefix: 'armor', columns: 3, dir: 'output'
   save_pdf trim: 37.5, dir: 'output'
